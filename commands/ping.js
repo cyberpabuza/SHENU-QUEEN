09ea71010afe5bb8835b84469b5a89ac8af58f19
DARK-SHADOW <124597615+MR-DARK-SHADOW@users.noreply.github.com>
2023-07-15 07:32:03 +0530

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -22,6 +22,6 @@ Secktor.cmd({
         const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
         var final = new Date().getTime();
        // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*🧚‍♀️ Pong*\n *' + (final - inital) + ' ms* ', edit: key});
+       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
     }
 );

6ad030932bfdc4f0e708b1ca5c1636c15e73342e
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-16 21:24:45 +0530

    SHENU QUEEN🧚

--- commands/_help.js
+++ commands/_help.js
@@ -92,7 +92,7 @@ Secktor.cmd({
                     image: { url: await botpic() },
                     caption: str,
                     footer: tlang().title,
-                    headerType: 10
+                    headerType: 1
                 };
                 return await Void.sendMessage(citel.chat, buttonMessaged);
             }

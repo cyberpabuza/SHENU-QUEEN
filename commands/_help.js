71f2a08a13408e7357577fa709008e6e38d8dbae
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-16 13:35:31 +0530

    SHENU QUEEN🧚‍♀️

--- commands/_help.js
+++ commands/_help.js
@@ -24,7 +24,7 @@ Secktor.cmd({
             alias: ["menu"],
             desc: "Help list",
             category: "general",
-            react: "👸",
+            react: "👼",
             filename: __filename
         },
         async(Void, citel, text) => {

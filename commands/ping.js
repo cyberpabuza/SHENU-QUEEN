196960b9314666865e8fbf93d1d038373fba36ae
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-19 03:54:07 +0530

    SHENU QUEEN🧚

--- commands/ping.js
+++ commands/ping.js
@@ -22,6 +22,6 @@ Secktor.cmd({
         const { key } = await Void.sendMessage(citel.chat, {text: '```Ping!!!```'});
         var final = new Date().getTime();
        // await Secktor.sleep(1000)
-       return await Void.sendMessage(citel.chat, {text: '*Pong*\n *' + (final - inital) + ' ms* ', edit: key});
+       return await Void.sendMessage(citel.chat, {text: '*🧙‍♀️Pong🧙‍♀️*\n *' + (final - inital) + '🔰 ms🔰* ', edit: key});
     }
 );

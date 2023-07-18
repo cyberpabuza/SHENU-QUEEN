89bc41999663d2f3c62e1f64b1c5c1bffbaf48ba
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-18 12:34:57 +0530

    SHENU QUEEN🧚

--- commands/downloader.js
+++ commands/downloader.js
@@ -564,73 +564,36 @@ cmd({
 
     }
 )
-
- //---------------------------------------------------------------------------
 cmd({
-            pattern: "audio",
-            alias :['song2'],
-            desc: "Downloads audio from youtube.",
+            pattern: "tgs",
+            desc: "Downloads telegram stickers.",
             category: "downloader",
             filename: __filename,
-            use: '<text>',
+            use: '<add sticker url.>'
         },
         async(Void, citel, text) => {
-            let yts = require("secktor-pack");
-            let search = await yts(text);
-            let anu = search.videos[0];
-            const getRandom = (ext) => {
-                return `${Math.floor(Math.random() * 10000)}${ext}`;
-            };
-            let infoYt = await ytdl.getInfo(anu.url);
-            if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`❌ Video file too big!`);
-            let titleYt = infoYt.videoDetails.title;
-            let randomName = getRandom(".mp3");
-            citel.reply = ('\t ╭──────Youtube Search Result─────╮  \n\n🏷Title : " + i.title + "\n\n🖇️Url : " + i.url +"\n\n🔖Description : " + i.timestamp +"\n\n👀Views : "+i.views +"\n\n📤Uploaded : " +i.ago +"\n\n👲🏻Author : "+i.author.name+"')
-            const stream = ytdl(anu.url, {
-                    filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
-                })
-                .pipe(fs.createWriteStream(`./${randomName}`));
-            await new Promise((resolve, reject) => {
-                stream.on("error", reject);
-                stream.on("finish", resolve);
-            });
-
-            let stats = fs.statSync(`./${randomName}`);
-            let fileSizeInBytes = stats.size;
-            let fileSizeInMegabytes = fileSizeInBytes / (1024 * 1024);
-            if (fileSizeInMegabytes <= dlsize) {
-                let buttonMessage = {
-                    document: fs.readFileSync(`./${randomName}`),
-                    mimetype: 'document/mpeg',
-                    fileName: titleYt + ".mp3",
-		    caption: `╭───────────────◆
-│◎◈◍ ${tlang().title} 
-┊ 🎶 *Youtube Player* 🎶
-│
-┊◎◈◍ *Title:* ${anu.title}
-│◎◈◍ *Duration:* ${anu.timestamp}
-┊◎◈◍ *Viewers:* ${anu.views}
-│◎◈◍ *Uploaded:* ${anu.ago}
-┊◎◈◍ *Author:* ${anu.author.name}
-╰────────────────◆
-⦿ *Url* : ${anu.url} `,  
-                    headerType: 4,
-                    contextInfo: {
-                        externalAdReply: {
-                            title: titleYt,
-                            body: citel.pushName,
-                            renderLargerThumbnail: true,
-                            thumbnailUrl: search.all[0].thumbnail,
-                            mediaUrl: text,
-                            mediaType: 1,
-                            thumbnail: await getBuffer(search.all[0].thumbnail),
-                            sourceUrl: text,
-                        },
-                    },
-                }
-                await Void.sendMessage(citel.chat, buttonMessage, { quoted: citel })
-                return fs.unlinkSync(`./${randomName}`);
-            } else {
-                citel.reply(`❌ File size bigger than 100mb.`);
-            }
-            fs.unlinkSync(`./${randomName}`);
+		if (!text) return await citel.reply("_Enter a tg sticker url_\nEg: .tgs https://t.me/addstickers/Oldboyfinal\nKeep in mind that there is a chance of ban if used frequently");
+		if (!text.includes("addstickers"))  return await citel.reply("_Uhh Please Enter a Valid tg sticker url_\nEg: .tgs https://t.me/addstickers/Oldboyfinal");
+		let tgUrl = text.split("|")[0];
+		let find = tgUrl.split("/addstickers/")[1];
+		let { result } = await fetchJson(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getStickerSet?name=${encodeURIComponent(find)} `);
+		let check = text.split("|")[1] || "";
+		let res = `Total stickers: ${result.stickers.length}\n*Estimated complete in:* ${result.stickers.length * 1.5} seconds\nKeep in mind that there is a chance of a ban if used frequently`.trim()
+		if (result.is_animated) return await citel.reply("Animated stickers are not supported");
+  		else if (check.startsWith("info")) return await citel.reply(res);
+		let limit = parseInt(check.split(",")[0]) || 10;
+		let count =  parseInt(check.split(",")[1]) ||  0;
+	 	let isCheckText = check.split(";")[1] ||  "Sticker"
+		let isSticker = true ;
+	        if (isCheckText.includes("photo") ){isSticker = false ;	isCheckText = "Photo"}
+		if(limit > result.stickers.length ) {  limit = result.stickers.length  }
+	        if(count > result.stickers.length ) {  count = result.stickers.length - 5  }
+		if(count > limit ){let temp = limit ;   limit = count;	count = temp ;}
+		await citel.reply(`${res}\n\n_Downloading as ${isCheckText} From index *${count}* to *${limit}*._\nIf you wants more to download then use Like \n\n .tgs ${tgUrl} |  10 ,  20 ; photo`)
+		for ( count ; count < limit ; count++) 
+		{
+		 // if (count >= limit) break;
+		  let file_path = await fetchJson(`https://api.telegram.org/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/getFile?file_id=${result.stickers[count].file_id}`);
+		  let sticUrl = `https://api.telegram.org/file/bot891038791:AAHWB1dQd-vi0IbH2NjKYUk-hqQ8rQuzPD4/${file_path.result.file_path}`;
+		  if(isSticker) { let a = await getBuffer(sticUrl); await citel.reply(a, { packname: Config.packname, author: "Suhail-Md"  }, "sticker");} 
+		  else { await Void.sendMessage(citel.chat,{image : {url : sticUrl } , caption : `*_Telegram Sticker At Index ${count+1} Downloaded_*`}) }

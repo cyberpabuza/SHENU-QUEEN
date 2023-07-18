93a05d541325f2fd697c97cd57f9dc5d81b7d093
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-19 01:59:16 +0530

    SHENU QUEEN🧚

--- commands/system.js
+++ commands/system.js
@@ -241,7 +241,7 @@ cmd({
     //---------------------------------------------------------------------------
 cmd({
             pattern: "alive",
-            react: "‍👋",
+            react: 🎀",
             category: "general",
             filename: __filename,
             desc: "is bot alive??"
@@ -278,7 +278,7 @@ _Powered by ${Config.ownername}_
 
                 audio: {
 
-                    url: "https://github.com/Pramesh04/sup_to_ravana/raw/main/Audio/alive.mp3",
+                    url: "https://gist.github.com/mrhansamala/3b02632b5a3ea91a534aee6cb402b863",
 
                 },
 

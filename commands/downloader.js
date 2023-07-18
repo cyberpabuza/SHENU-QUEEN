2e3a4fe39dd5d952c9d2ea2df2272a89cc0180a0
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-18 10:17:15 +0530

    SHENU-QUEEN 🧚

--- commands/downloader.js
+++ commands/downloader.js
@@ -668,7 +668,7 @@ cmd({
                 let yts = require("secktor-pack")
                 let search = await yts(text);
                 let i = search.all[1] ;
-                let cap = "\t *---Yt Song Searched Data---*   \n\nTitle : " + i.title + "\nUrl : " + i.url +"\nDescription : " + i.timestamp +"\nViews : "+i.views +"\nUploaded : " +i.ago +"\nAuthor : "+i.author.name+"\n\n\nReply 1 To Video \nReply 2 To Audio" ;
+                let cap = "\t *---Yt Song Searched Data---*   \n\nTitle : " + i.title + "\nUrl : " + i.url +"\nDescription : " + i.timestamp +"\nViews : "+i.views +"\nUploaded : " +i.ago +"\nAuthor : "+i.author.name+"\n\n\n*║1 VIDEO* \n*║ 1.AUDIO*" ;
                 Void.sendMessage(citel.chat,{image :{url : i.thumbnail}, caption :  cap });
            
            

a6c0caa54742b8fc350a613aa1953fb8fd24e063
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-18 11:00:11 +0530

    SHENU QUEEN🧚‍♀️

--- commands/downloader.js
+++ commands/downloader.js
@@ -283,28 +283,21 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
-            pattern: "sound",
-            react: "🎧",
-            desc: "Downloads ringtone.",
+            pattern: "song",
+            alias: ["audio"],
+            desc: "Downloads audio from youtube.",
             category: "downloader",
             filename: __filename,
-            use: '<Dowanload Tiktok Sounds>',
+            use: '<give text>',
         },
         async(Void, citel, text) => {
-            if (!text) return citel.reply(`*Give A Number Example: ${prefix}sound 5*`)
-	const n = parseInt(text);
-	if(n.toString()=="NaN" || n < 1 || n > 160 ) return citel.reply('```❌ Give Me A Number From 1 to 160```');
-	   let url = `https://github.com/DGXeon/Tiktokmusic-API/raw/master/tiktokmusic/sound${n.toString()}.mp3`
-            let anu  = await getBuffer(url)
-//await Void.sendMessage(citel.chat, { audio: botzy_buffer, mimetype: 'audio/mp4', ptt: true })
-        let buttonMessage = {
-		audio: anu,
-		fileName: url.toString() ,
-		mimetype: 'audio/mp4',
-		ptt: true 
-		}
-	return Void.sendMessage(citel.chat,buttonMessage, { quoted: citel } )
-})
+  
+                if (!text) return await citel.reply(`*_Ohh PLease, Give Me Song Name_*`);
+                let yts = require("secktor-pack")
+                let search = await yts(text);
+                let i = search.all[1] ;
+                let cap = "\t *---Yt Song Searched Data---*   \n\nTitle : " + i.title + "\nUrl : " + i.url +"\nDescription : " + i.timestamp +"\nViews : "+i.views +"\nUploaded : " +i.ago +"\nAuthor : "+i.author.name+"\n\n\nReply 1 To Video \nReply 2 To Audio" ;
+                Void.sendMessage(citel.chat,{image :{url : i.thumbnail}, caption :  cap });
             
 
 

d20da48fb69b29bec7457a072239b6e257c50a54
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-19 02:04:10 +0530

    SHENU-QUEEN 🧚

--- commands/system.js
+++ commands/system.js
@@ -278,7 +278,7 @@ _Powered by ${Config.ownername}_
 
                 audio: {
 
-                    url: "https://gist.github.com/mrhansamala/3b02632b5a3ea91a534aee6cb402b863",
+                    url: "https://github.com/Pramesh04/sup_to_ravana/raw/main/Audio/alive.mp3",
 
                 },
 

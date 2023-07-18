3cb1cac8364589d00bc7f308dd30e67010c281d3
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-18 12:49:00 +0530

    SHENU QUEEN🧚

--- commands/downloader.js
+++ commands/downloader.js
@@ -301,7 +301,7 @@ cmd({
             if (infoYt.videoDetails.lengthSeconds >= videotime) return citel.reply(`❌ Video file too big!`);
             let titleYt = infoYt.videoDetails.title;
             let randomName = getRandom(".mp3");
-            citel.reply('*Downloadig:* '╭───────────────◆
+            citel.reply('*╭───────────────◆
 │◎◈◍ ${tlang().title} 
 ┊ 🎶 *Youtube Player* 🎶
 │
@@ -311,7 +311,7 @@ cmd({
 │◎◈◍ *Uploaded:* ${anu.ago}
 ┊◎◈◍ *Author:* ${anu.author.name}
 ╰────────────────◆
-⦿ *Url* : ${anu.url})
+⦿ *Url* : ${anu.url}');
             const stream = ytdl(anu.url, {
                     filter: (info) => info.audioBitrate == 160 || info.audioBitrate == 128,
                 })

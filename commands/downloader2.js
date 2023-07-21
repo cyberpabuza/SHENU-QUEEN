71756565a1f3e22d80d8a8fd665fa848b40a1aeb
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-21 15:40:44 +0530

    SHENU QUEEN🧚‍♂️

--- commands/downloader2.js
+++ commands/downloader2.js
@@ -320,51 +320,46 @@ const videolink = fbdls.result.audio
 
     )
 
+const { tlang, botpic, cmd, prefix, runtime, Config, formatp, fetchJson } = require('../lib')
+const { download} = require('aptoide-scraper')
 cmd({
-
-            pattern: "apk",
-
-            desc: "apk down",
-
-            react: "🗃️",
-
-            category: "downloader"
-
+    pattern: "downapk",
+    alias: ["ps","apk","playstore"],
+    desc: "download playstore app",
+    react: "💾",
+    category: "downloader",
+    filename: __filename,
+},
+async (Void, citel, text) => {
+if (!text) return
+try {
+let result = await download(text)
+ const applink = result.dllink
+    const getname = result.name
+    const icon = result.icon
+    const lastupdate = result.lastup
+    const packagename = result.package
+    const size = result.size
+      await Void.sendMessage(citel.chat, {
+        image: {
+            url: icon,
         },
-
-        async(Void, citel, text) => {
-
-            if (!text) return
-
-const apkdl = await fetchJson(`https://legend-army-api.onrender.com/api/dowloader/aptoide?text=${text}&apikey=83801daa`)
-
-            citel.reply (`*Hello ${citel.pushName} I Am Finding Your Apk*`);
-
-const applink = apkdl.data.dllink
-
-const getname = apkdl.data.name
-
-const lastupdate = apkdl.data.lastup
-
-       return Void.sendMessage(citel.chat, {
-
-                document: {
-
-                    url: applink ,
-
-                },
-
-                mimetype: "application/vnd.android.package-archive",
-
-                caption: tlang().footer,
-
-                fileName: getname ,
-
-            }, {
-
-                quoted: citel,
-
-            });
+        caption: `*Hello ${citel.pushName} , _Please wait...._ ,I Am Finding Your Apk File*\n╭─────🎀\n│🧙‍♀️${tlang().title}\n│★ *Playstore Download*\n│★ *App name:* ${getname}\n│★ *Last update:* ${lastupdate}\n│★ *Package name:* ${packagename}\n│★ *File size:* ${size}\n╰──────────🎀\n⦿ *You can also download the App from the link below* 👇: \n${applink}`,
+    })
+    return Void.sendMessage(citel.chat, {
+        document: {
+            url: applink,
+        },
+        mimetype: "application/vnd.android.package-archive",
+        fileName: getname,
+    }, {
+        quoted: citel,
+    });
+  } catch (err) {
+    console.error(err);
+    citel.reply(`❌ An error occurred while processing your request. Please try again later.${err}`);
+  }
+})
 
         }
 

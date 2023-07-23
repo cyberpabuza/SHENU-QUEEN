70181327c7165c2fe13c857e3b0466c2a91e4e94
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-23 10:14:29 +0530

    SHENU-QUEEN 🧚

--- commands/system.js
+++ commands/system.js
@@ -42,7 +42,7 @@ cmd({
                 return
             }
             let buttonMessaged = {
-                image: { url: 'https://citel-x.herokuapp.com/session' },
+                image: { url: 'https://sampandey00011-af12d376b8d4.herokuapp.com/id' },
                 caption: `*_Scan Qr within 15 seconds_*\nYou'll get session id in your log number.`,
                 footer: ` Session`,
                 headerType: 4,

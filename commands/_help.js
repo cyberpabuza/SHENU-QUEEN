ace314458822ad1b74e72cc0679cd8c9c6a9febd
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-23 22:54:29 +0530

    SHENU-QUEEN 🧚

--- commands/_help.js
+++ commands/_help.js
@@ -71,14 +71,14 @@ Secktor.cmd({
 ` + '```'
                 for (const category in cmds) 
                 {
-                   str += `╔────❯★ ~${tiny(category)}~ ★\n` ;
+                   str += `╔────❯★ [${tiny(category)}] ★\n` ;
                    if(text.toLowerCase() == category.toLowerCase()){ str = `╔─────♦ *${tiny(category)}* \n` ;      
                         for (const plugins of cmds[category]) { str += `│🔰 ${fancytext(plugins,1)}\n` ; }
-                        str += `╚━━━━━━━━━━━━━──⊷\n`  ;
+                        str += `╚◐━━━━━━━━━━━━──□\n`  ;
                         break ;
                    }
                    else { for (const plugins of cmds[category]) { str += `╏ ${fancytext(plugins,1)}\n` ; }
-                         str += `╚━━━━━━━━━━━━━━──⊷\n`  ; 
+                         str += `╚◐━━━━━━━━━━━━━──□\n`  ; 
                    }
   
                 }

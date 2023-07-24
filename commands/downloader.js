2d8e6e43d4e9c02a94de2b76c27ff6327a30ee4e
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-25 04:41:14 +0530

    SHENU QUEEN🧚

--- commands/downloader.js
+++ commands/downloader.js
@@ -434,7 +434,7 @@ if (text.startsWith("https://youtube.com/shorts/")) {
                         externalAdReply: {
                             title: titleYt,
                             body: citel.pushName,
-                            renderLargerThumbnail: false,
+                            renderLargerThumbnail: true,
                             thumbnailUrl: search.all[0].thumbnail,
                             mediaUrl: anu.url,
                             mediaType: 1,

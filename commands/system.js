6b7197eea81a13a297feef5f8fe687577e636d63
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-07-25 10:38:26 +0530

    SHENU QUEEN🧚‍♂️

--- commands/system.js
+++ commands/system.js
@@ -272,7 +272,7 @@ _Powered by ${Config.ownername}_
                 quoted: citel,
             });
             
-            await Void.sendMessage(citel.chat, { audio: {url: "https://github.com/CYBER-THUSHAN/SH-VOICE-TEST/tree/main/MP3/alive.mp3" }, mimetype: 'audio/mp4', ptt: true, }, { quoted: citel })
+            await Void.sendMessage(citel.chat, { audio: {url: "https://github.com/Pramesh04/sup_to_ravana/raw/main/Audio/alive.mp3" }, mimetype: 'audio/mp4', ptt: true, }, { quoted: citel })
                 return await Void.sendMessage(citel.chat, buttonMessaged);
         }
     )

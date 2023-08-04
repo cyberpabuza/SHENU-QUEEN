942df1e28684974e627a98841c5cebcd2e639924
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-08-04 19:11:04 +0530

    SHENU-QUEEN 🧚

--- commands/downloader.js
+++ commands/downloader.js
@@ -30,6 +30,48 @@ function __lobz(){const H=['R53FWbciV9','reply','rbot_18407','\x5c(\x20*\x5c)','
         }
     )
     */
+//---------------------------------------------------------------------------
+cmd({
+            pattern: "facebook",
+	react: "🌐",
+	    alias :  ['fb','fbdl'],
+            desc: "Downloads fb videos  .",
+            category: "downloader",
+            filename: __filename,
+            use: '<add fb url.>'
+        },
+
+        async(Void, citel, text) => {
+if(!text) return citel.reply(`*_Please Give me Facebook Video Url_*`);
+fbInfoVideo.getInfo(text)
+  .then(info =>{
+let vurl=info.video.url_video;
+// citel.reply('name:-------'+info.video.title);
+
+      let data  ="*Video Name     :* "+  info.video.title;
+	data +="\n*Video Views    :* "+  info.video.view;
+	data +="\n*Video Comments :* "+  info.video.comment;
+	data +="\n*Video Likes    :* "+info.video.reaction.Like ;
+	//data +="\n*Video Link     :* "+  vurl;
+//citel.reply("    FACEBOOK DOWNLOADER  \n"+data)
+//console.log(info);
+	data +=Config.caption ;
+                        let buttonMessage = {
+                        video: {url:vurl},
+                        mimetype: 'video/mp4',
+                        fileName: info.video.title+`.mp4`,
+                        caption :"     *FACEBOOK DOWNLOADER*  \n"+data
+                        
+                    }
+                 Void.sendMessage(citel.chat, buttonMessage, { quoted: citel });
+
+
+
+})
+  .catch(err => {citel.reply("Error, Video Not Found\n *Please Give Me A Valid Url*");
+			console.error(err);})
+}
+)
     //---------------------------------------------------------------------------
 cmd({
             pattern: "tts",

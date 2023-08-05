521c35a4f0c73d64f151df32351332ec51d64382
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-08-06 02:37:58 +0530

    SHENU-QUEEN 🧚

--- commands/downloader.js
+++ commands/downloader.js
@@ -743,15 +743,6 @@ if (text.startsWith("https://youtube.com/shorts/")) {
                     fileName: titleYt + ".mp3",
                     headerType: 4,
                     contextInfo: {
-                        externalAdReply: {
-                            title: titleYt,
-                            body: citel.pushName,
-                            renderLargerThumbnail: false,
-                            thumbnailUrl: search.all[0].thumbnail,
-                            mediaUrl: anu.url,
-                            mediaType: 1,
-                            thumbnail: await getBuffer(search.all[0].thumbnail),
-                            sourceUrl: anu.url,
                         },
                     },
                 }

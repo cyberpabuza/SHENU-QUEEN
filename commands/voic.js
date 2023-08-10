ec4c904eda9bfde4addede2059d3089df7b891ce
CYBER-THUSHAN <107739528+CYBER-THUSHAN@users.noreply.github.com>
2023-08-10 11:43:55 +0530

    SHENU QUEEN🧚‍♂️

--- commands/voic.js
+++ commands/voic.js
@@ -1,5 +1,5 @@
-const url = 'https://raw.githubusercontent.com/CYBER-THUSHAN/VOICE-TEST/main/Mp3/shenu%20v1/Hi.mp3'
+const url = 'https://gist.github.com/prabathLK/f602911954a959c8730aeb00a588d15d/raw'
   let { data } = await axios.get(url)
   for (vr in data){
-  if((new RegExp(`\\b${vr}\\b`,'Hi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
-  }
+  if((new RegExp(`\\b${vr}\\b`,'gi')).test(body)) conn.sendMessage(from,{audio: { url : data[vr]},mimetype: 'audio/mpeg',ptt:true},{quoted:mek})   
+    }
